887ebf868f9cadf399327b6332d6d91597860b52
Sumanth Benhur <sbenhur2@gmail.com>
2021-01-31 14:06:21 +0530

    css changes font

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const multer =require("multer");
 const path=require("path");
 const { resolveSoa } = require("dns");
-const popup = require('popups');
+let alert = require("alert");
 
 const app = express();
 //to connect js and css files in public
@@ -220,7 +220,7 @@ app.get("/product/:productId", function(req, res){
     product.deleteOne({_id : requestedProductId}, function(err, post){
       Info.findOne({email : req.user.username}, function(err, result){
         if(err) console.log(err);
-        else console.log(result);
+        else alert("Item Deleted");
         res.redirect("/profile");
       });
     });
@@ -256,10 +256,8 @@ console.log(userdetails);
 
     User.register({username: req.body.username}, req.body.password, function(err, user){
       if (err) {
-        console.log(err);
-        //alert("Email already taken");
-        res.redirect("/register");
-        
+        alert("E-mail already taken");
+        res.redirect("/register");        
         
       } else {
         
@@ -281,7 +279,7 @@ console.log(userdetails);
   
     req.login(user, function(err){
       if (err) {
-        console.log(err);
+        alert("Wrong user credentials")
       } else {
         passport.authenticate("local")(req, res, function(){
           res.redirect("/index");
@@ -333,7 +331,7 @@ console.log(userdetails);
       });
       item.save();
     });
-   
+   alert("Item added");
     res.redirect("index"); 
 
     
